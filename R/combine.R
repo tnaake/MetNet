77968cb74f83047c7f61e262623941c84e72e45d
tnaake <thomasnaake@gmx.de>
2021-03-29 11:51:18 +0200

    update combine to accomodate AdjacencyMatrix objects

--- R/combine.R
+++ R/combine.R
@@ -2,101 +2,107 @@
 #'
 #' @aliases combine
 #'
-#' @title Combine structural and statistical adjacency matrix
+#' @title Combine structural and statistical `AdjacencyMatrix` objects
 #'
 #' @description
 #' The function `combine` takes as
-#' input the structural and statistical adjacency matrix, created in former
-#' steps, adds them together and will report a connection between metabolites
-#' in the returned when the sum exceeds the `threshold`.
-#' \code{combine} returns this consensus matrix supported
-#' by the structural and statistical adjacency matrices.
+#' input the structural and statistical `AdjacencyMatrix` objects, created in former
+#' steps. It will access the assays `binary` and `consensus`, adds them 
+#' together and will report a connection between metabolites
+#' if the edge is present in both matrices. 
+#' 
+#' `combine` returns an `AdjacencyMatrix` containing this consensus matrix 
+#' supported by the structural and statistical adjacency matrices (assays
+#' `combine_binary`, `combine_transformation`, and `combine_mass_difference`.
 #'
-#' @param structural list containing `numeric` structural adjacency matrix in
-#' the first entry and `character` structural adjanceny matrix in the second
-#' entry
+#' @param am_structural `AdjacencyMatrix` containing `numeric` structural 
+#' adjacency matrices (assays `binary`, `transformation`, and 
+#' `mass_difference`).
 #'
-#' @param statistical matrix containing `numeric` statistical adjacency matrix
+#' @param am_statistical `AdjacencyMatrix` containing the assay `consensus`
 #'
-#' @param threshold numeric, threshold value to be applied to define a
-#' connection as present
+#' @details The matrices from the assays `binary` and `consensus` will be added 
+#' and an unweighted connection will
+#' be reported when the edges are respectively present in both `binary` and 
+#' `consensus`.
 #'
-#' @details The matrices will be added and a unweighted connection will
-#' be reported when the value exceeds a certain value.
-#'
-#' @return `list`, in the first entry `matrix` of type `numeric`containing the
-#' consensus adjacency matrix as described
-#' above harbouring connections reported by the structual and
-#' statistcal adjacency matrices. In the second entry a `matrix` of type
-#' `character` the corresonding type/putative link at this position.
-#'
+#' @return 
+#' `AdjacencyMatrix` object containing the assays
+#' `combine_binary` (`numeric` adjacency matrix), 
+#' `combine_transformation` (`character` adjacency matrix), and 
+#' `combine_mass_difference` (`character` adjacency matrix). 
+#' 
+#' The `AdjacencyMatrix` object will also contain all other assays contained
+#' in `am_structural` and `am_statistical`.
+#' 
 #' @author Thomas Naake, \email{thomasnaake@@googlemail.com}
 #'
 #' @examples
 #' data("x_test", package = "MetNet")
 #' x_test <- as.matrix(x_test)
-#' functional_groups <- rbind(
+#' transformation <- rbind(
 #'     c("Monosaccharide (-H2O)", "C6H10O5", "162.0528234315"),
 #'     c("Disaccharide (-H2O)", "C12H20O11", "340.1005614851"),
 #'     c("Trisaccharide (-H2O)", "C18H30O15", "486.1584702945"))
-#' functional_groups <- data.frame(group = functional_groups[, 1],
-#'      formula = functional_groups[, 2],
-#'      mass = as.numeric(functional_groups[, 3]))
-#' struct_adj <- structural(x_test, functional_groups, ppm = 5)
-#' stat_adj_l <- statistical(x_test,
-#'     model = c("pearson", "spearman"),
+#' transformation <- data.frame(group = transformation[, 1],
+#'      formula = transformation[, 2],
+#'      mass = as.numeric(transformation[, 3]))
+#'      
+#' am_struct <- structural(x_test, transformation, ppm = 5)
+#' am_stat <- statistical(x_test, model = c("pearson", "spearman"),
 #'     correlation_adjust = "bonferroni")
-#' stat_adj <- threshold(stat_adj_l, type = "top2", args = list(n = 10))
-#' combine(struct_adj, stat_adj)
+#' 
+#' am_stat <- threshold(am_stat, type = "top2", args = list(n = 10))
+#' combine(am_structural = am_struct, am_statistical = am_stat)
 #'
 #' @export
-combine <- function(structural, statistical, threshold = 1) {
+combine <- function(am_structural, am_statistical) {
 
-    if (!is.list(structural) | length(structural) != 2)
-        stop("structural is not a list of length 2")
-
-    if (!is.matrix(structural[[1]]) | !is.numeric(structural[[1]]))
-        stop("structural[[1]] is not a numeric matrix")
-
-    if (!is.matrix(structural[[2]]) | !is.character(structural[[2]]))
-        stop("structural[[2]] is not a character matrix")
-
-    if (!is.matrix(statistical) | !is.numeric(statistical))
-        stop("statistical is not a numeric matrix")
-
-    if (!all(rownames(structural[[1]]) == rownames(structural[[2]])))
-        stop(c("rownames of structural[[1]] are not identical to rownames of ",
-             "structural[[2]]"))
-
-    if (!all(colnames(structural[[1]]) == colnames(structural[[2]])))
-        stop(c("colnames of structural[[1]] are not identical to colnames of ",
-                 "structural[[2]]"))
-
-    if (!all(rownames(structural[[1]]) == rownames(statistical)))
-        stop("rownames are not identical")
-
-    if (!all(colnames(structural[[1]]) == colnames(statistical)))
-        stop("colnames are not identical")
-
-    if (!is.numeric(threshold)) stop("threshold is not numeric")
-
+    if (!is(am_structural, "AdjacencyMatrix")) 
+        stop("'am_structural' is not an 'AdjacencyMatrix' object")
+    
+    if (!validObject(am_structural)) 
+        stop("'am_structural' must be a valid 'AdjacencyMatrix' object")
+    
+    if (!is(am_statistical, "AdjacencyMatrix")) 
+        stop("'am_structural' is not an 'AdjacencyMatrix' object")
+    
+    if (!validObject(am_statistical)) 
+        stop("'am_structural' must be a valid 'AdjacencyMatrix' object")
+    
+    if (!("consensus" %in% assayNames(am_statistical))) 
+        stop("'am_statistical' must contain assay 'consensus'")
+    
     ## create list to store results
     res <- list()
 
     ## create the first entry of the list
     ## sum the matrices structural and statistical, if the value is above
     ## threshold then assign 1, otherwise 0
-    cons_num <- structural[[1]] + statistical
-    cons_num <- ifelse(cons_num > threshold, 1, 0)
-
+    mat_bin <- assay(am_structural, "binary") + assay(am_statistical, "consensus")
+    mat_bin <- ifelse(mat_bin > 1, 1, 0)
+    
     ## create the second entry of the list
-    ## if element in cons_num is equal to 1, take the element in structural[[2]]
-    ## (the type of link), otherwise ""
-    cons_char <- ifelse(cons_num == 1, structural[[2]], "")
-
-    ## assign to list
-    res[[1]] <- cons_num
-    res[[2]] <- cons_char
-
-    return(res)
+    ## if element in mat_bin is equal to 1, take the element in 
+    ## the assay "transformation" (the type of link), otherwise ""
+    mat_type <- ifelse(mat_bin == 1, assay(am_structural, "transformation"), "")
+    
+    ## create the third entry of the list
+    ## if element in mat_bin is equal to 1, take the element in 
+    ## the assay "mass_difference" (the mass difference of link), otherwise ""
+    mat_mass <- ifelse(mat_bin == 1, assay(am_structural, "mass_difference"), "")
+    
+    ## create the AdjacencyMatrix object
+    l <- list(combine_binary = mat_bin, combine_transformation = mat_type, 
+        combine_mass_difference = mat_mass)
+    rD <- DataFrame(names = rownames(mat_bin), row.names = rownames(mat_bin))
+    
+    directed <- if (directed(am_structural) | directed(am_statistical)) TRUE else FALSE
+    thresholded <- if (thresholded(am_structural) | thresholded(am_statistical)) TRUE else FALSE
+    
+    am <- AdjacencyMatrix(l, rowData = rD, type = "combine", 
+        directed = directed, thresholded = thresholded)
+    
+    return(am)
+    
 }
